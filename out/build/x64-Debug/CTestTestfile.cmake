cc3d2f2afb63469635e1c80c498142df5cdd84a4
Abhijith1399 <150343745+Abhijith1399@users.noreply.github.com>
2025-07-09 23:51:08 +0530

    update

--- out/build/x64-Debug/CTestTestfile.cmake
+++ out/build/x64-Debug/CTestTestfile.cmake
@@ -4,4 +4,5 @@
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
-subdirs("test")
+add_test(CalculatorTest "C:/Users/Administrator/Desktop/MyCalculator/My_Calculator/out/build/x64-Debug/CalculatorTest.exe")
+set_tests_properties(CalculatorTest PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/Administrator/Desktop/MyCalculator/My_Calculator/CMakeLists.txt;27;add_test;C:/Users/Administrator/Desktop/MyCalculator/My_Calculator/CMakeLists.txt;31;add_calc_test;C:/Users/Administrator/Desktop/MyCalculator/My_Calculator/CMakeLists.txt;0;")
